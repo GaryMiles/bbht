7770f58793557eda859ff1b7f5301ee5d070d570
GaryMiles <59375100+GaryMiles@users.noreply.github.com>
2020-01-05 13:26:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,54 +23,27 @@ git clone https://github.com/nahamsec/recon_profile.git
 cd recon_profile
 cat bash_profile >> ~/.bash_profile
 source ~/.bash_profile
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
-
-
-#install go
-if [[ -z "$GOPATH" ]];then
-echo "It looks like go is not installed, would you like to install it now"
-PS3="Please select an option : "
-choices=("yes" "no")
-select choice in "${choices[@]}"; do
-        case $choice in
-                yes)
-
-					echo "Installing Golang"
-					wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
-					sudo tar -xvf go1.13.4.linux-amd64.tar.gz
-					sudo mv go /usr/local
-					export GOROOT=/usr/local/go
-					export GOPATH=$HOME/go
-					export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-					echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
-					echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
-					echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
-					source ~/.bash_profile
-					sleep 1
-					break
-					;;
-				no)
-					echo "Please install go and rerun this script"
-					echo "Aborting installation..."
-					exit 1
-					;;
-	esac	
-done
-fi
-
-
-#Don't forget to set up AWS credentials!
-echo "Don't forget to set up AWS credentials!"
-apt install -y awscli
-echo "Don't forget to set up AWS credentials!"
-
-
+echo "Installing Golang"
+wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+sudo tar -xvf go1.13.4.linux-amd64.tar.gz
+sudo mv go /usr/local
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
+source ~/.bash_profile
+sleep 1
+break
+;;
 
 #create a tools folder in ~/
 mkdir ~/tools
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 
 #install aquatone
 echo "Installing Aquatone"
@@ -86,20 +59,20 @@ echo "installing JSParser"
 git clone https://github.com/nahamsec/JSParser.git
 cd JSParser*
 sudo python setup.py install
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 echo "installing Sublist3r"
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r*
 pip install -r requirements.txt
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 
 echo "installing teh_s3_bucketeers"
 git clone https://github.com/tomdev/teh_s3_bucketeers.git
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 
@@ -107,39 +80,39 @@ echo "installing wpscan"
 git clone https://github.com/wpscanteam/wpscan.git
 cd wpscan*
 sudo gem install bundler && bundle install --without test
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 echo "installing dirsearch"
 git clone https://github.com/maurosoria/dirsearch.git
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 
 echo "installing lazys3"
 git clone https://github.com/nahamsec/lazys3.git
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 echo "installing virtual host discovery"
 git clone https://github.com/jobertabma/virtual-host-discovery.git
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 
 echo "installing sqlmap"
 git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 echo "installing knock.py"
 git clone https://github.com/guelfoweb/knock.git
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 echo "installing lazyrecon"
 git clone https://github.com/nahamsec/lazyrecon.git
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 echo "installing nmap"
@@ -148,16 +121,16 @@ echo "done"
 
 echo "installing massdns"
 git clone https://github.com/blechschmidt/massdns.git
-cd ~/tools/massdns
+cd /content/drive/My\ Drive/Linux/tools/massdns
 make
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 echo "installing asnlookup"
 git clone https://github.com/yassineaboukir/asnlookup.git
-cd ~/tools/asnlookup
+cd /content/drive/My\ Drive/Linux/tools/asnlookup
 pip install -r requirements.txt
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 echo "installing httprobe"
@@ -177,16 +150,14 @@ git clone https://github.com/nahamsec/crtndstry.git
 echo "done"
 
 echo "downloading Seclists"
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 git clone https://github.com/danielmiessler/SecLists.git
-cd ~/tools/SecLists/Discovery/DNS/
+cd /content/drive/My\ Drive/Linux/tools/SecLists/Discovery/DNS/
 ##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
 cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
-cd ~/tools/
+cd /content/drive/My\ Drive/Linux/tools/
 echo "done"
 
 
 
 echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
-ls -la
-echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
